27b1a3999b8712d9c1d948c20418704724e4b169
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-16 11:19:40 -0600

    feat: structure

---
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.css'
+})
+export class ProductPageComponent {
+
+}
