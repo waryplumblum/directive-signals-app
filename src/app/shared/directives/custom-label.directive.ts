72021714db1a02e0027c900246015b04f0241c99
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-16 12:57:22 -0600

    feat: directive input

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,9 +6,14 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color:string = 'red';
+
+  @Input() set color(value:string){
+    this._color=value;
+    this.setStyle();
+  }
 
   constructor( private el:ElementRef<HTMLElement>) {
-    console.log(el);
     this.htmlElement =  el;
   }
 
@@ -16,4 +21,8 @@ export class CustomLabelDirective implements OnInit{
     console.log('Directiva - NgOnInit');
   }
 
+  setStyle():void{
+    if( !this.htmlElement ) return;
+    this.htmlElement!.nativeElement.style.color = this._color;
+  }
 }
