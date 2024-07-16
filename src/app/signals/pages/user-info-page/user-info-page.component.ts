9c9a596037c01ce3481fb7bf2376f3fabd7db6a4
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-16 16:58:04 -0600

    feat: manejo de erorres y propiedades conmutadas

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -12,6 +12,10 @@ export class UserInfoPageComponent implements OnInit{
   public userId       = signal(1);
   public currentUser  = signal<User|undefined>(undefined);
   public userWasFound = signal(true);
+  public fullName     = computed<string> ( () => {
+    if(!this.currentUser() ) return 'Usuario no encontrado.';
+    return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  })
 
   constructor(
     private usersService:UsersServiceService
@@ -28,9 +32,16 @@ export class UserInfoPageComponent implements OnInit{
     this.currentUser.set(undefined);
 
     this.usersService.getUserById( id )
-      .subscribe( user => {
-        this.currentUser.set(user)
-      })
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set(user);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false),
+          this.currentUser.set(undefined);
+        },
+      });
   }
 
 }
