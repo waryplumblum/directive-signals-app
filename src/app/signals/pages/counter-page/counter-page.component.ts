a33c737edde49366a9a227dbc9e3e93b160de900
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-16 16:16:04 -0600

    feat: counter signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter()*this.counter());
+
+  increaseBy(value:number){
+    this.counter.update(current => current + value)
+  }
 
 }
