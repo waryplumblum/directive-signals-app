1c5a929e9a08427dbc9e70512504c188ed32ee71
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-16 18:00:52 -0600

    feat: mutations

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -7,9 +8,39 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 1,
+    email: 'george.blu@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  });
 
-  onFieldUpdated(field:string,value:string) {
-    console.log({ field,value });
+
+  onFieldUpdated(field:keyof User,value:string) {
+
+    this.user.update( current => {
+
+      switch(field){
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return current;
+    });
 
   }
 
